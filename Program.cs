f40e2703390878c8ee4d246af52703101bd0ca89
Rodrigo Rocha <devrsrocha@hotmail.com>
2020-07-28 21:10:34 -0300

    Aplicação console .NET criada.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace consoleApp_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroRepeticoes = 5;
+
+            for (int i = 0; i < numeroRepeticoes; i++)
+            {
+                Console.WriteLine($"Loop {i}");
+            }
+
+            
+        }
+    }
+}
